501c69f3c01558922ef288c7355f9911eaa0f167
Raquel <raquelsglez99@gmail.com>
2024-06-18 17:18:10 +0200

    ejercicio-usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState('Sofía');
+    const [newName, setNewName] = useState('');
+
+    const changeNameClick = (newName) => {
+        setName(newName);
+    };
+
+    const changeName = (event) => {
+        event.preventDefault(); // -> para evitar que el formulario recargue la página, ya que sin esto se 
+        //recarga al darle al botón porque es de tipo submit
+        
+        if (newName != '') {
+            setName(newName);
+            setNewName('');
+        };
+    };
+
+    return (
+        <div>
+            <h2>Teacher Name: {name} </h2>
+            <ul>
+                <li onClick={() => changeNameClick('Data')}>Data</li>
+                <li onClick={() => changeNameClick('Reyes')}>Reyes</li>
+                <li onClick={() => changeNameClick('Yolanda')}>Yolanda</li>
+            </ul>
+            <form onSubmit={changeName}>
+                <input type="text" placeholder="add a name" value={newName} onChange={event => {setNewName(event.target.value)}}/>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
